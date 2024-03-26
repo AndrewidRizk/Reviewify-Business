cb0ed486ca85ca81592ba39cec928427f68595d5
Andrew Rizk <97995173+AndrewidRizk@users.noreply.github.com>
2024-03-26 09:14:25 -0400

    Done and working

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from places import main_get_total_reviews
+from cohere_api import classify_reviews, summarize_reviews
+app = Flask(__name__)
+CORS(app) # Enable CORS for all routes
+
+@app.route('/flask/process_data', methods=['POST'])
+def process_data():
+    data = request.json  # Access JSON data sent from React
+
+    # Print the data to the console
+    print("Data from React:", data)
+
+    # You can also log it to a file if needed
+    with open("data.log", "a") as f:
+        f.write(str(data) + "\n")
+
+    # Perform further processing if needed
+    # Example: Extracting specific fields from the data
+    restaurant = data.get('restaurant', '') # getting the restaurant name
+    postal = data.get('postal', '') #getting the restaurant postal code
+    reviews_from_places = main_get_total_reviews(restaurant_name=restaurant, postal_code=postal) # getting all the reviews
+    
+    # Calling the cohere functions
+    # returning the classified results inputing the reviews and getting positive, negatiev, and unrelated
+    classified_result, pos, neg, unrel = classify_reviews(reviews_from_places) 
+    # samarize the code
+    summary = summarize_reviews(classified_result)
+
+    print(f"classified_result {classified_result}")
+    print(f"-------------------pos-----------------\n {pos}")
+    print(f"-------------------neg-----------------\n {neg}")
+    print(f"-------------------unrel-----------------\n {unrel}")
+    print("------------------------------------------------")
+    print(f"summary {summary}")
+
+    # Return a response
+    
+    positive = (pos/5)*100
+    negative = (neg/5)*100
+    unrelated = (unrel/5)*100
+
+    # Construct response data
+    response_data = {
+        "resultStatus": "Success",
+        'message': [positive, negative, unrelated, summary[0], summary[1]]
+    }
+    
+    # Return processed data
+    return jsonify(response_data)
+
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ places.py
@@ -0,0 +1,73 @@
+import requests
+import pickle
+from restaurant_reviews import Restaurant
+import os
+
+API_KEY = os.environ.get('api_key_Y2')
+
+def get_reviews(restaurant):
+    place_id = get_place_id(restaurant.name, restaurant.location)
+
+    if place_id:
+        reviews = get_reviews_text_by_place_id(place_id)
+        return reviews
+    else:
+        return None
+
+def get_place_id(restaurant_name, location):
+    url = f'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={restaurant_name} {location}&inputtype=textquery&key={API_KEY}'
+    response = requests.get(url)
+    data = response.json()
+
+    if response.status_code == 200 and 'candidates' in data and data['candidates']:
+        return data['candidates'][0]['place_id']
+    else:
+        return None
+
+def get_reviews_text_by_place_id(place_id):
+    url = f'https://maps.googleapis.com/maps/api/place/details/json?place_id={place_id}&key={API_KEY}'
+    response = requests.get(url)
+    data = response.json()
+
+    if response.status_code == 200 and 'result' in data and 'reviews' in data['result']:
+        reviews_data = data['result']['reviews'][:10]
+        review_texts = [review['text'] for review in reviews_data]
+        return review_texts
+    else:
+        return None
+
+
+def get_location_from_postal_code(postal_code, api_key):
+    base_url = "https://maps.googleapis.com/maps/api/geocode/json"
+    params = {
+        "address": f"{postal_code}, Canada",
+        "key": api_key,
+    }
+    response = requests.get(base_url, params=params)
+    data = response.json()
+
+    if response.status_code == 200 and data.get("results"):
+        formatted_address = data["results"][0]["formatted_address"]
+        return formatted_address
+    else:
+        return f"Location not found for postal code {postal_code}"
+
+def main_get_total_reviews(restaurant_name, postal_code):
+    # Input: Name of the restaurant and postal code
+    #restaurant_name = 'Pita N More'
+    #postal_code = 'L8S 1C5'
+
+    # Get location from postal code
+    location = get_location_from_postal_code(postal_code, API_KEY)
+
+    # Create a Restaurant instance
+    restaurant = Restaurant(restaurant_name, postal_code, location)
+
+
+
+
+    # Get reviews from Places API
+    reviews_from_places = get_reviews(restaurant)
+
+
+    return reviews_from_places
