2685baddbb1a9af40eee43bbedff3df48936ffcf
Andrew Rizk <97995173+AndrewidRizk@users.noreply.github.com>
2025-02-17 20:21:08 -0500

    Revert "pop up for start"
    This reverts commit df1e7fc1fd14262edd59c6ec0d156b7633eba7f0.

--- my-app/src/components/Searchbar.js
+++ my-app/src/components/Searchbar.js
@@ -9,47 +9,36 @@ const SearchBar = (props) => {
   const [showPopup, setShowPopup] = useState(false); // Controls pop-up visibility
 
   const sendDataToFlask = async () => {
-    // Indicate loading status
     props.setLoading(true);
-    
-    // Show pop-up immediately when search starts
-    setShowPopup(true);
+    setShowPopup(true); // Show pop-up when search starts
 
-    // Hide the pop-up after 3 seconds
     setTimeout(() => {
-      setShowPopup(false);
+      setShowPopup(false); // Hide pop-up after 3 seconds
     }, 3000);
 
-    // API endpoint
     const apiUrl = 'https://reviewify-backend.vercel.app/flask/process_data';
 
-    // Data to be sent
     const requestData = {
       restaurant: restaurant,
       postal: postal,
     };
 
     try {
-      // Sending the POST request
       const response = await axios.post(apiUrl, requestData);
-      // Handling the response
       props.setProcessedData(response.data.message);
     } catch (error) {
       console.error('Error:', error);
     } finally {
-      // Reset loading status
       props.setLoading(false);
     }
   };
 
-  // If the component is in a loading state, don't show the search bar
   if (props.loading === true) {
     return <span></span>;
   }
 
   return (
     <div className="new-search">
-      {/* Input for restaurant name */}
       <input
         type="text"
         value={restaurant}
@@ -57,7 +46,6 @@ const SearchBar = (props) => {
         placeholder="Business Name"
         className="searchInput"
       />
-      {/* Input for postal code */}
       <input
         type="text"
         value={postal}
@@ -65,10 +53,9 @@ const SearchBar = (props) => {
         placeholder="Postal code"
         className="searchInput"
       />
-      {/* Search button triggers the API call and pop-up */}
       <button onClick={sendDataToFlask} id="searchButton">Search</button>
 
-      {/* Conditionally rendered pop-up notification */}
+      {/* Pop-up Notification */}
       {showPopup && <div className="popup">Try the app</div>}
     </div>
   );
